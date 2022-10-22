6db1f6a5848fbe5d4ab5643040154d47fadeed9d
ravi kumar <86415295+meravikumar@users.noreply.github.com>
2022-10-22 16:09:21 +0530

    Number pattern

---
+++ pattern1.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r,c;
+    cin>>r>>c;
+    int i =0;
+    while(i<r){
+        int j=0;
+        while(j<c){
+            cout<<i;
+            j++;
+        }
+    cout<<endl;
+      i++ ;
+    }
+
+    return 0;
+}
+
